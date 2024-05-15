beaf4f2da470f5d0f2e30c47462ff7de1e174029
TsukuiMasahito <166583662+35280TsukuiMasahito@users.noreply.github.com>
2024-05-15 13:27:18 +0900

    toupper

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,13 +8,19 @@ using System.Threading.Tasks;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var list = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-            //int count = numbers.Count(n => n % 2 == 0);
-            double avg = numbers.Where(n=>n>5).Average();
-            int num = numbers.Where(n=>n>5).Sum();
-            Console.WriteLine(num);
-            Console.WriteLine(avg);
+            var lowerList = list.ConvertAll(s => s.ToUpper());
+            lowerList.ForEach(s => Console.WriteLine(s));
         }
     }
 }
