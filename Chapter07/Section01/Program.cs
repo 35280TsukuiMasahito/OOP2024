03400dbb115878a605ac0b0602a4202042c50fdb
TsukuiMasahito <166583662+35280TsukuiMasahito@users.noreply.github.com>
2024-06-11 15:52:00 +0900

    fin

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -1,106 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Section01 {
     internal class Program {
+        static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
 
         static void Main(string[] args) {
 
-            var kenchodict = new Dictionary<String, String>();
-            for (int i = 0; i < 5; i++) {
-                Console.Write("都道府県:");
-                var Key = Console.ReadLine();
-                Console.Write("県庁所在地:");
-                var Value = Console.ReadLine();
+            String pref, prefCaptalLocation;
 
-                if (kenchodict.ContainsKey(Key)) {
-                    Console.Write("上書きしますか？(y/n)");
-                    var reWrite = Console.ReadLine();
-                    if (reWrite == "n") {
-                        //上書き
+            //入力処理
+            Console.WriteLine("県庁所在地の登録");
+            while (true) {
+                //都道府県の入力
+                Console.Write("都道府県:");
+                pref = Console.ReadLine();
+
+                if (pref == null) {
+                    break;
+                }
+
+                //県庁所在地の入力
+                Console.Write("県庁所在地:");
+                prefCaptalLocation = Console.ReadLine();
+
+                //既に都道府県が登録されているか？
+                if (prefOfficeDict.ContainsKey(pref)) {
+                    //登録済み
+                    Console.WriteLine("上書きしますか？(Y/N)");
+                    if (Console.ReadLine() == "N") {
                         continue;
                     }
                 }
-                kenchodict[Key] = Value;
+                prefOfficeDict[pref] = prefCaptalLocation;  //登録
             }
-            //foreach (var n in kenchodict) {
-            //    Console.WriteLine(n.Key + "の県庁所在地は" + n.Value + "です");
-            //}
+            Console.WriteLine();//改行
 
-            String getnum;
-            int num;
+            Boolean endFlag = false;    //終了フラグ（無限ループを抜け出す用）
+            while (!endFlag) {
+                switch (menuDisp()) {
+                    case "1":
+                        //一覧出力処理
+                        allDisp();
+                        break;
 
-            do {
-                Console.WriteLine("*メニュー*");
-                Console.WriteLine("1:一覧表示");
-                Console.WriteLine("2:検索");
-                Console.WriteLine("9:終了");
-                Console.Write("数値を入力:");
-                getnum = Console.ReadLine();
-                num = int.Parse(getnum);
-                if (num == 1) {
-                    foreach (var n in kenchodict) {
-                        Console.WriteLine(n.Key + "の県庁所在地は" + n.Value + "です");
-                    }
-                } else if (num == 2) {
-                    Console.Write("検索する都道府県:");
-                    String s = Console.ReadLine();
-                    if (kenchodict.ContainsKey(s)) {
-                        foreach (var n in kenchodict) {
-                            if (n.Key == s) {
-                                Console.WriteLine(n.Key + "の県庁所在地は" + n.Value + "です");
-                            }
-                        }
-                    } else {
-                        Console.WriteLine("見つかりません");
-                    }
+                    case "2":
+                        //検索処理
+                        searchPrefCaptalLocation();
+                        break;
 
+                    case "9":
+                        endFlag = true; //終了フラグＯＮ
+                        break;
                 }
-            } while (num != 9);
-            Console.WriteLine("処理を終了します");
-            return;
+            }
+        }
+        //検索処理
+        private static void searchPrefCaptalLocation() {
+            Console.Write("都道府県:");
+            String searchPref = Console.ReadLine();
+            Console.WriteLine(searchPref + "の県庁所在地は" + prefOfficeDict[searchPref] + "です");
         }
 
+        //一覧表示処理
+        private static void allDisp() {
+            foreach (var item in prefOfficeDict) {
+                Console.WriteLine("{0}の県庁所在地は{1}です。", item.Key, item.Value);
+            }
+        }
 
-        //var employeeDict = new Dictionary<int, Employee> {
-        //   { 100, new Employee(100, "清水遼久") },
-        //   { 112, new Employee(112, "芹沢洋和") },
-        //   { 125, new Employee(125, "岩瀬奈央子") },
-        //};
-
-        //var flowerDict = new Dictionary<string, int>() {
-        //      { "sunflower", 400 },
-        //      { "pansy", 300 },
-        //      { "tulip", 350 },
-        //      { "rose", 500 },
-        //      { "dahlia", 450 },
-        //};
-        ////Console.WriteLine(flowerDict["sunflower"]);
-        ////Console.WriteLine(flowerDict["dahlia"]);
-
-        //employeeDict.Add(126, new Employee(126, "茂木愼")); //追加
-
-        //var name = employeeDict.Where(e => e.Value.Name.Contains("和"));
-
-        //foreach(var item  in employeeDict.Values) {
-        //    Console.WriteLine($"{item.Id} {item.Name}");
-        //}
-
-        //var emp0 = employeeDict[100];
-        //Console.WriteLine($"{emp0.Id} {emp0.Name}"); //取り出し
-        //var emp1 = employeeDict[112];
-        //Console.WriteLine($"{emp1.Id} {emp1.Name}");
-        //var emp2 = employeeDict[125];
-        //Console.WriteLine($"{emp2.Id} {emp2.Name}");
-        //var emp3 = employeeDict[125];
-        //Console.WriteLine($"{emp3.Id} {emp3.Name}");
-
-        //var result = employeeDict.Remove(126); //消去
-
+        //メニュー表示
+        private static string menuDisp() {
+            Console.WriteLine("**** メニュー ****");
+            Console.WriteLine("1：一覧表示");
+            Console.WriteLine("2：検索");
+            Console.WriteLine("9：終了");
+            Console.Write(">");
+            String menuSelect = Console.ReadLine();
+            return menuSelect;
+        }
     }
 }
-
