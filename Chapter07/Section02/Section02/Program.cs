4ba7bf58ab355fae6b73df28b31ae80f280c0cfd
TsukuiMasahito <166583662+35280TsukuiMasahito@users.noreply.github.com>
2024-06-18 10:28:21 +0900

    7-1

---
+++ Chapter07/Section02/Section02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dict = new Dictionary<MonthDay, string> {
+                { new MonthDay(3, 5), "珊瑚の日" },
+                { new MonthDay(8, 4), "箸の日" },
+                { new MonthDay(10, 3), "登山の日" },
+           };
+            var md = new MonthDay(8, 4);
+            var s = dict[md];
+            Console.WriteLine(s);
+        }
+
+
+        }
+    }
