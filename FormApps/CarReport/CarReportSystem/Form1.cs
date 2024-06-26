f807749b0b48ee3f5108bef7cf7a1e1c955d6780
TsukuiMasahito <166583662+35280TsukuiMasahito@users.noreply.github.com>
2024-06-26 15:50:31 +0900

    クラス作成

--- FormApps/CarReport/CarReportSystem/Form1.cs
+++ FormApps/CarReport/CarReportSystem/Form1.cs
@@ -2,18 +2,6 @@ namespace CarReportSystem {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
-        }
 
-        private void label1_Click(object sender, EventArgs e) {
-
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e) {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e) {
-
-        }
     }
 }
