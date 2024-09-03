4398bf6146e5cf16d6dee9be6fc40565833ce2e3
TsukuiMasahito <166583662+35280TsukuiMasahito@users.noreply.github.com>
2024-09-03 11:26:47 +0900

    open the web

---
+++ FormApps/RSSReader/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace RSSReader {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbRssUrl = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbRssTitle = new System.Windows.Forms.ListBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // tbRssUrl
+            // 
+            this.tbRssUrl.Location = new System.Drawing.Point(27, 23);
+            this.tbRssUrl.Name = "tbRssUrl";
+            this.tbRssUrl.Size = new System.Drawing.Size(509, 19);
+            this.tbRssUrl.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(567, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "取得";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbRssTitle
+            // 
+            this.lbRssTitle.FormattingEnabled = true;
+            this.lbRssTitle.ItemHeight = 12;
+            this.lbRssTitle.Location = new System.Drawing.Point(27, 70);
+            this.lbRssTitle.Name = "lbRssTitle";
+            this.lbRssTitle.Size = new System.Drawing.Size(509, 172);
+            this.lbRssTitle.TabIndex = 2;
+            this.lbRssTitle.SelectedIndexChanged += new System.EventHandler(this.lbRssTitle_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(27, 264);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(761, 310);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 586);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.lbRssTitle);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbRssUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRssUrl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lbRssTitle;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ FormApps/RSSReader/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+
+namespace RSSReader {
+    public partial class Form1 : Form {
+        private Dictionary<string, string> rssItems = new Dictionary<string, string>();
+
+
+        public Form1() {
+            InitializeComponent();
+            // イベントハンドラーの登録
+            lbRssTitle.SelectedIndexChanged += lbRssTitle_SelectedIndexChanged;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+                using (var wc = new WebClient()) {
+                    var url = wc.OpenRead(tbRssUrl.Text);
+                    var xdoc = XDocument.Load(url);
+
+                    // リストボックスをクリア
+                    lbRssTitle.Items.Clear();
+                    rssItems.Clear();
+
+                    var items = xdoc.Descendants("item");
+                    foreach (var item in items) {
+                        var title = item.Element("title")?.Value;
+                        var link = item.Element("link")?.Value;
+
+                        if (!string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(link)) {
+                            lbRssTitle.Items.Add(title);
+                            rssItems[title] = link; // タイトルとリンクのペアを保存
+                        }
+                    }
+                }
+            
+        }
+
+        private void lbRssTitle_SelectedIndexChanged(object sender, EventArgs e) {
+            if (lbRssTitle.SelectedItem != null) {
+                var selectedTitle = lbRssTitle.SelectedItem.ToString();
+                if (rssItems.TryGetValue(selectedTitle, out var url)) {
+                        webBrowser1.Navigate(url);
+                    webBrowser1.ScriptErrorsSuppressed = true;
+                }
+            }
+        }
+    }
+}
