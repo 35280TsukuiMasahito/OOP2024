9c20460241de55b9a4a793110759516966b52a51
TsukuiMasahito <166583662+35280TsukuiMasahito@users.noreply.github.com>
2024-10-03 11:40:58 +0900

    section03完成

--- Section01/Program.cs
+++ Section01/Program.cs
@@ -30,30 +30,97 @@ namespace Section01 {
             Console.WriteLine(max);
         }
 
-        private static void Exercise1_3() { 
+        private static void Exercise1_3() {
             var quary = Library.Books
-                .GroupBy(b=>b.PublishedYear)
-                .Select(x=>new {Publisher =})
+                .GroupBy(b => b.PublishedYear)
+                .Select(x => new { Publisher = x.Key,Count = x.Count()})
+                .OrderBy(x=>x.Publisher);
+            
+            foreach (var x in quary) {
+                Console.WriteLine("{0}年 {1}冊",x.Publisher,x.Count);
+            }
         }
 
         private static void Exercise1_4() {
-            throw new NotImplementedException();
+            var query = Library.Books
+                .Join(Library.Categories,
+                    book => book.CategoryId,
+                    Category => Category.Id,
+                    (book, Category) => new {
+                        book.Title,
+                        book.PublishedYear,
+                        book.Price,
+                        CategoryName = Category.Name,
+                    }).OrderByDescending(x => x.PublishedYear)
+                    .ThenByDescending(x => x.Price);
+
+            foreach(var x in query) {
+                Console.WriteLine("{0}年 {1}円 {2}", x.PublishedYear, x.Price, x.CategoryName);
+            }
         }
 
+
         private static void Exercise1_5() {
-            throw new NotImplementedException();
+            var query = Library.Books
+                .Where(b => b.PublishedYear == 2016)
+                .Join(Library.Categories, //結合する2番目のシーケンス
+                book => book.CategoryId, //対象シーケンスの結合キー
+                Category => Category.Id, //結合する2番目の結合キー
+                (book, Category) => Category.Name)
+                .Distinct();
+
+            foreach (var x in query) {
+                Console.WriteLine(x);
+            }
         }
 
         private static void Exercise1_6() {
-            throw new NotImplementedException();
+            var query = Library.Books
+                        .Join(Library.Categories,
+                            book => book.CategoryId,
+                            Category => Category.Id,
+                            (book, Category) => new {
+                                book.Title,
+                                CategoryName = Category.Name
+                            })
+                        .GroupBy(x => x.CategoryName)
+                        .OrderBy(x=>x.Key);
+
+            foreach(var x in query) {
+                Console.WriteLine("#{0}",x.Key);
+                foreach(var item  in x) {
+                    Console.WriteLine("  {0}",item.Title);
+                }
+            }
         }
 
         private static void Exercise1_7() {
-            throw new NotImplementedException();
+            var categoresId = Library.Categories.Single(x=>x.Name == "Depelopment").Id;
+            var query = Library.Books.Where(x=>x.CategoryId == categoresId)
+                .GroupBy(x=>x.PublishedYear)
+                .OrderBy(x=>x.Key);
+
+            foreach (var x in query) {
+                Console.WriteLine("#{0}年",x.Key);
+                foreach(var item in x) {
+                    Console.WriteLine("  {0}", item.Title);
+                }
+            }
         }
 
         private static void Exercise1_8() {
-            throw new NotImplementedException();
+            var query = Library.Categories
+                .GroupJoin(Library.Books,
+                            c => c.Id,
+                            b => b.CategoryId,
+                            (c, b) => new { CategoryName = c.Name,
+                                            Count = b.Count()
+                            })
+                .Where(x=>x.Count >= 4);
+
+            foreach (var item in query) {
+                Console.WriteLine(item.CategoryName + "("+item.Count + "冊");
+            }
         }
     }
 }
