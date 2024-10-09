08f10b279fe3b9143c047396307b82f2e5f2c390
TsukuiMasahito <166583662+35280TsukuiMasahito@users.noreply.github.com>
2024-10-09 14:57:26 +0900

    パターン作成テンプレメソッド

---
+++ Chapter17/Section01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var greetings = new List<GreetingBase> {
+                new GreetingMorning(),
+                new GreetingAfternoon(),
+                new GreetingEvening(),
+            };
+        }
+    }
+}
---
+++ Chapter17/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace LineCounter {
+     class LineCounterProcessor : TextProcessor{
+        private int _count;
+        string _text = "";
+
+        protected override void Initialize(string fname) {
+            _text += fname;
+            _count = 0;
+        }
+        protected override void Execute(string line) {
+            _count++;
+        }
+        protected override void Terminate() {
+            Console.WriteLine("{0}",_count);
+            Console.WriteLine(_text);
+        }
+    }
+}
