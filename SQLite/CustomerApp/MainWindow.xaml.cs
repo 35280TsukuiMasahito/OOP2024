b003dbeca5f7fd2477abeb13d786b6096605f8de
TsukuiMasahito <166583662+35280TsukuiMasahito@users.noreply.github.com>
2024-11-14 12:08:15 +0900

    起動時にread

--- SQLite/CustomerApp/MainWindow.xaml.cs
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -20,8 +20,10 @@ namespace CustomerApp {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        List<Customer> _customers;
         public MainWindow() {
             InitializeComponent();
+            ReadDatabase();
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e) {
@@ -35,16 +37,41 @@ namespace CustomerApp {
                 connection.CreateTable<Customer>();
                 connection.Insert(customer);
             }
+            ReadDatabase();
         }
 
         private void ReadButton_Click(object sender, RoutedEventArgs e) {
 
+        }
+
+        private void ReadDatabase() {
             using (var connection = new SQLiteConnection(App.databasePass)) {
                 connection.CreateTable<Customer>();
-                var customers = connection.Table<Customer>().ToList();
+                _customers = connection.Table<Customer>().ToList();
 
-                CustomerListView.ItemsSource = customers;
-                
+                CustomerListView.ItemsSource = _customers;
+
+            }
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            var filterList = _customers.Where(x=>x.Name.Contains(SearchTextBox.Text)).ToList();
+            CustomerListView.ItemsSource = filterList;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+            var item = CustomerListView.SelectedItem as Customer;
+            if(item == null) {
+                MessageBox.Show("削除する行を選択してください");
+                return;
+            }
+
+            using (var connection = new SQLiteConnection(App.databasePass)) {
+                connection.CreateTable<Customer>();
+                connection.Delete(item);
+
+                CustomerListView.ItemsSource = _customers;
+
             }
         }
     }
